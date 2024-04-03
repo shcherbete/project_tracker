17ea1be3c73cc0cd4d1f8ddd60e109341c90b3db
Ekaterina Shcherbinina <kingerkatek@gmail.com>
2024-04-03 22:51:11 +0300

    admin settings + start of homework

--- quality_control/admin.py
+++ quality_control/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
 # Register your models here.
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'project', 'task', 'status', 'priority', 'created_at')
+    list_filter = ('status', 'project', 'task', 'priority')
+    search_fields = ('title', 'description')
+    ordering = ('created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+
+
+
+# Класс администратора для модели Task
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'project', 'task', 'status', 'priority', 'created_at')
+    list_filter = ('status', 'project', 'task', 'priority')
+    search_fields = ('title', 'description')
+    list_editable = ('status', 'priority')
+    readonly_fields = ('created_at', 'updated_at')
