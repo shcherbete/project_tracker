c1c7ecf7d08f31a5e15180dc60799ffd6611d1ff
Ekaterina Shcherbinina <kingerkatek@gmail.com>
2024-04-24 13:08:59 +0300

    admin settings + start of homework

--- quality_control/admin.py
+++ quality_control/admin.py
@@ -7,6 +7,7 @@ class BugReportAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'project', 'task', 'status', 'priority', 'created_at')
     list_filter = ('status', 'project', 'task', 'priority')
     search_fields = ('title', 'description')
+    list_editable = ('status', 'priority')
     ordering = ('created_at',)
     readonly_fields = ('created_at', 'updated_at')
     date_hierarchy = 'created_at'
